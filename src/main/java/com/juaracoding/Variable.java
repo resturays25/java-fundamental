68f545a2869f2fddd26c4f70aa6d1e6ff4fd947c
Ray <resturays@gmail.com>
2024-07-31 19:14:17 +0700

    java fundamental 1,2

---
+++ src/main/java/com/juaracoding/Variable.java
@@ -0,0 +1,80 @@
+package com.juaracoding;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        //tipe data number
+//        byte age = 127;
+//        short stock = 9999;
+//        float price = 10000.55f;
+//        double totalPrice = 3000000000.99d;
+//        long total = 40000000L;
+//        int cartProduct = 1000;
+//
+//        //char = hanya menampung 1 character
+//        char gender = 'L';
+//        char predikat = 'A';
+
+        //escape character
+//        System.out.println("Juara Coding\nIndonesia");
+//        System.out.println("Juara Coding\tIndonesia");
+//        System.out.println("\"Juara Coding\tIndonesia");
+
+        //boolean
+//        boolean isResult = true;
+//        boolean isLogin = false;
+//        boolean isAvailable = true;
+
+        //String
+//        String productName = "Headset";
+//        System.out.println("Product Name "+productName);
+
+        //buatkan output menampilkan profile user
+//        String name = "Ray";
+//        byte usia = 28;
+//        String email = "ray@gmail.com";
+//        boolean isSubscribe = true;
+//        char jk = 'L';
+//        long pembelian = 3200000000L;
+//        System.out.println("Nama : "+name);
+//        System.out.println("Usia : "+usia+" Tahun");
+//        System.out.println("Email : "+email);
+//        System.out.println("Apakah Berlangganan? "+ isSubscribe);
+//        System.out.println("Total Belanja: "+pembelian);
+//        System.out.println(name+ " Adalah Pelanggan Tetap\nNomor 1 DI TOKO INI");
+
+//        String pathImage = "C:\\Users\\User\\Downloads\\evidence.jpg";
+//        System.out.println(pathImage);
+
+        //Casting Variable
+        //Implicit Casting / Automatic Casting (Promotion)
+//        short data1 = 456;
+//        double dataDouble = data1;
+//        System.out.println(data1);
+//        System.out.println(dataDouble);
+//
+//        char data2 = 'Z';
+//        long dataLong = data2;
+//        System.out.println(dataLong);
+
+        //Explicit Casting / Manual Casting
+//        char data3 = '+';
+//        byte dataByte = (byte) data3;
+//        System.out.println(dataByte);
+//
+//        int harga = 9999; //android
+//        short dataShort = (short) harga; //java backend
+//        System.out.println(dataShort);
+
+        //Passing by Value
+//        String address = "Jakarta";
+//        String duplicateAddress = address;
+//        duplicateAddress = "Bandung";
+//        System.out.println(duplicateAddress); //hasilnya Bandung karena dibaca dari baris perbaris sampai menampilkan yang terakhir
+
+        //phi=3.14 data static tidak bisa dipassing
+
+        //Passing by Reference
+
+    }
+}
